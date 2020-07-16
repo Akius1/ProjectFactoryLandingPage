8bccd8f462fefabf7799654053d612eb914cf5ba
Akius1 <andrewurom@gmail.com>
2020-07-16 00:18:28 -0700

    Made some changes to session and master section

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const nav_mobile = document.querySelector(".nav-menu-mobile");
 
 
 const showNav = () =>{
-    console.log('i am working');
     nav_mobile.classList.toggle("show");
 }
 hamburger.addEventListener("click", showNav);
